8cfe2efb858bf6784fc9050499f60db6c2cee688
byJonyB <by.jonyb@gmail.com>
2013-05-14 07:59:56 +0200

    Modificaciones en la GUI.

--- src/gui/Main.java
+++ src/gui/Main.java
@@ -2,11 +2,14 @@ package gui;
 
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
-import java.awt.Menu;
-import java.awt.MenuBar;
-import java.awt.MenuItem;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
 
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 
 @SuppressWarnings("serial")
@@ -38,34 +41,90 @@ public class Main extends JFrame {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		
-		/* Creación de la barra de menús. */
-		MenuBar mbarra = new MenuBar();
-        Menu m = new Menu("Archivo");
-        /*m.add(new MenuItem("Nuevo"));
-        m.add(new MenuItem("Abrir"));
-        m.add(new MenuItem("Guardar"));
-        m.add(new MenuItem("Guardar como"));
-        m.add(new MenuItem("Imprimir"));
-        m.addSeparator();*/
-        m.add(new MenuItem("Salir"));
-        mbarra.add(m);
-        m = new Menu("Mostrar");
-        m.add(new MenuItem("Artistas"));
-        m.add(new MenuItem("Eventos"));
-        m.addSeparator();
-        m.add(new MenuItem("Panel de usuario"));
-        mbarra.add(m);
-        m = new Menu("Ayuda");
-        m.add(new MenuItem("Ayuda"));
-        m.addSeparator();
-        m.add(new MenuItem("Acerca de PureSound"));
-        mbarra.add(m);
-        setMenuBar(mbarra);
+		/* Ponemos la barra de menú. */
+        ponerMenu();
         
         /* Añadimos el panel al frame principal. */
-		contentPane = new JPanel();
-		contentPane.add(new Carga(), BorderLayout.CENTER);
-		setContentPane(contentPane);
+        cambiarPanel(new Carga());
+	}
+	
+	private void ponerMenu() {
+		JMenuBar menuBar = new JMenuBar();
+
+        /* Menú Archivo */
+        JMenu menu = new JMenu("Archivo");
+        menu.setMnemonic(KeyEvent.VK_F);
+        menuBar.add(menu);
+
+        /* Submenú Archivo */
+        JMenuItem item = new JMenuItem("Salir");
+        menu.add(item);
+        item.addActionListener(new MenuSalirActionListener());
+        
+        /* Menú Mostrar */
+        menu = new JMenu("Mostrar");
+        menuBar.add(menu);
+
+        /* Submenú Mostrar */
+        item = new JMenuItem("Artistas");
+        menu.add(item);
+        item.addActionListener(new MenuArtistasActionListener());
+        
+        item = new JMenuItem("Eventos");
+        menu.add(item);
+        item.addActionListener(new MenuEventosActionListener());
+        
+        menu.addSeparator(); // Separador entre submenús.
+        
+        item = new JMenuItem("Panel de usuario");
+        menu.add(item);
+        item.addActionListener(new MenuUsuarioActionListener());
+        
+        /* Menú Ayuda */
+        menu = new JMenu("Ayuda");
+        menuBar.add(menu);
+
+        /* Submenú Ayuda */
+        item = new JMenuItem("Ayuda");
+        menu.add(item);
+        
+        menu.addSeparator();
+        
+        item = new JMenuItem("Acerca de PureSound");
+        menu.add(item);
+        
+        /* Añadimos la barra de menú al frame principal. */
+        super.setJMenuBar(menuBar);
+	}
+	
+	private class MenuSalirActionListener implements ActionListener {
+		public void actionPerformed(ActionEvent e) {
+			System.exit(0);
+		}
+	}
+	
+	private class MenuArtistasActionListener implements ActionListener {
+		public void actionPerformed(ActionEvent e) {
+			cambiarPanel(new Artistas());
+		}
+	}
+	
+	private class MenuEventosActionListener implements ActionListener {
+		public void actionPerformed(ActionEvent e) {
+			cambiarPanel(new Eventos());
+		}
+	}
+	
+	private class MenuUsuarioActionListener implements ActionListener {
+		public void actionPerformed(ActionEvent e) {
+			cambiarPanel(new User());
+		}
 	}
 
+	private void cambiarPanel(JPanel pPanel) {
+		contentPane = new JPanel();
+		contentPane.add(pPanel, BorderLayout.CENTER);
+		setContentPane(contentPane);
+		contentPane.updateUI();
+	}
 }
